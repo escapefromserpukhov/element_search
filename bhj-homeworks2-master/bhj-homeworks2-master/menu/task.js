7c03bb3401c36fb2d3f0310cf03f91dd7f33e98b
Viktoria_finch <85229737+escapefromserpukhov@users.noreply.github.com>
2023-03-30 11:47:37 +0300

    Update task.js

--- bhj-homeworks2-master/bhj-homeworks2-master/menu/task.js
+++ bhj-homeworks2-master/bhj-homeworks2-master/menu/task.js
@@ -1,17 +1,26 @@
-(() => {
-    const menu = document.body.querySelectorAll(".menu__link");
-    for (let i = 0; i < menu.length; i++) {
-        menu[i].addEventListener("click", myFunction);
-    }
-    function myFunction() {
-        this.nextElementSibling.onclick = function() {
-            return false;
-        }
-        if (this.nextElementSibling.className === 'menu menu_sub menu_active') {
-            this.nextElementSibling.classList.remove("menu_active");
-        } else {
-            this.nextElementSibling.classList.add("menu_active");
-        }
-    };
-})();
+'use strict';
 
+const menu_link = document.getElementsByClassName('menu__link');
+
+for (let i = 0; i < menu_link.length; i++)
+  menu_link[i].onclick = function() {
+    
+    const menu_main = this.closest('ul');
+    const menu_sub = this.parentElement.querySelector('.menu_sub');
+    
+    if (menu_main.className === 'menu menu_main') {
+      const menu_active = menu_main.querySelector('.menu_active');
+      if (menu_active && (menu_active !== menu_sub)) 
+        menu_active.className = 'menu menu_sub';
+    }
+
+    if (!menu_sub) 
+      return true;
+    
+    if (menu_sub.className === 'menu menu_sub') 
+      menu_sub.className = 'menu menu_sub menu_active';
+    else
+      menu_sub.className = 'menu menu_sub';   
+    return false;
+       
+  }
